53d5ddb24c972a4e30bb82f4b241e449af15703f
Adebayo114 <matthewadebayo392@gmail.com>
2025-05-12 12:01:09 +0100

    final build and updates

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/legacy-of-Adebayo/",
+  base: "/Lga/",
   plugins: [react()],
 });
